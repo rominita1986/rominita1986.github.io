6b627e82c6b372f9a5cd4c274da6f74bb0398392
rominita1986 <126616296+rominita1986@users.noreply.github.com>
2023-02-28 18:21:58 -0300

    initial commit

---
+++ function.js
@@ -0,0 +1,13 @@
+
+
+
+document.getElementById('boton').addEventListener('click', function()  {
+        
+        document.getElementById("demo").innerHTML = "Los feos no adjuntamos foto de perfil. Nos daña el CV";
+
+    });
+
+
+    
+
+
